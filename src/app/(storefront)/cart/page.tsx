91650738f37ed78fe5e83f3b6b37716bf951d964
deyvisonborges <deyvisonsilvaborges@gmail.com>
2025-04-24 23:55:40 -0300

    working with useLinkStatus in header links

--- src/app/(storefront)/cart/page.tsx
+++ src/app/(storefront)/cart/page.tsx
@@ -1,5 +1,8 @@
 import { CartPage } from "@/modules/store/features/cart/pages/cart.page"
 
-export default function CartAppPage() {
+export default async function CartAppPage() {
+  // Simula 2 segundos de delay
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
   return <CartPage />
 }
--- src/modules/store/layouts/store-layout/components/header/index.tsx
+++ src/modules/store/layouts/store-layout/components/header/index.tsx
@@ -1,14 +1,30 @@
 "use client"
 
-import { Heading, Input } from "@/@lib-ui"
+import { Heading, Input, Spinner } from "@/@lib-ui"
 import styles from "./header.module.css"
 import { CartIconBlack24, UserIconBlack24 } from "@/@lib-icons/src"
-import Link from "next/link"
+import Link, { useLinkStatus } from "next/link"
 import { Navigation } from "./navigation"
 import { useCart } from "@/modules/store/features/cart/use-cart"
+import { usePathname } from "next/navigation"
 
 export function Header() {
   const { cartItems } = useCart()
+  const pathname = usePathname()
+  const isCurrentPage = pathname === "/cart"
+
+  function CartIconWithStatus({ value }: { value: number }) {
+    const { pending } = useLinkStatus()
+
+    return (
+      <div style={{ position: "relative", height: "24px", width: "24px" }}>
+        <CartIconBlack24 value={value} />
+        <div style={{ position: "absolute", top: "0", right: "-64px" }}>
+          {pending && <Spinner size="small" color="blue" />}
+        </div>
+      </div>
+    )
+  }
 
   return (
     <header className={styles.header}>
@@ -23,8 +39,18 @@ export function Header() {
 
       <div className={styles["header-right"]}>
         <Input name="search" placeholder="Busque o que procura" />
-        <Link href="/cart" prefetch={false}>
-          <CartIconBlack24 value={cartItems.length} />
+        {/* Por padrão, o Next.js pré-carrega (pré-busca) a página de destino quando você passa o mouse em cima de um <Link />. Isso acelera a navegação porque os dados já estão no cache quando você clica. */}
+        {/* Quando você coloca prefetch={false}, você desativa esse comportamento de pré-carregamento automático. */}
+        <Link
+          href="/cart"
+          prefetch={false}
+          onClick={(e) => {
+            if (isCurrentPage) {
+              e.preventDefault()
+            }
+          }}
+        >
+          <CartIconWithStatus value={cartItems.length} />
         </Link>
         <Link href="/auth/login">
           <UserIconBlack24 />
